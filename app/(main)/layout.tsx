4b58d78bddbaa688cc355e9a057ebb9725fddf1f
richardbeducation <richardbeducation@gmail.com>
2024-08-24 07:43:47 +0700

    update

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import { Sidebar } from '@/components/Sidebar'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const MainLayout = ({ children }: Props) => {
+  return (
+    <>
+      <Sidebar className='hidden lg:flex' />
+      <main className='lg:pl-[256px] h-full'>
+        <div className='bg-red-500 h-full'>{children}</div>
+      </main>
+    </>
+  )
+}
+
+export default MainLayout
