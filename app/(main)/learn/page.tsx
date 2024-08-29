ef5079017788a9fcd04870432bc69ae92b519e3f
RichardB <richardbeducation@gmail.com>
2024-08-29 18:06:38 +0700

    update

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,20 @@ import { FeedWrapper } from '@/components/FeedWrapper'
 import { StickyWrapper } from '@/components/StickyWrapper'
 import { Header } from './Header'
 import { UserProgress } from '@/components/UserProgress'
+import { getUserProgress } from '@/db/queries'
+import { redirect } from 'next/navigation'
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress()
+
+  // promise all will wait for all the promises listed
+  const [userProgress] = await Promise.all([userProgressData])
+
+  // redirect to courses page if there's no user
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses')
+  }
+
   return (
     <div className='flex flex-row-reverse gap-[48px] px-6'>
       <StickyWrapper>
--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,29 @@
 import { cache } from 'react'
 import db from './drizzle'
+import { auth } from '@clerk/nextjs/server'
+import { eq } from 'drizzle-orm'
+import { userProgress } from './schema'
 
+// Drizzle query to to get from the courses table
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany()
+  return data
+})
+
+// Drizzle query to to get from the userProgress table
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth()
+
+  if (!userId) {
+    return null
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    // query to find the userProgress table that matches the userId from Clerk auth
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  })
   return data
 })
